09602d3d9c37d1590eac6908e89d9d1e564257df
M. J. Brodzik <brodzik@nsidc.org>
2016-04-11 11:22:11 -0600

    Latest stuff from worsley.

--- charis/dehra_dun/review_ablation_surfaces.py
+++ charis/dehra_dun/review_ablation_surfaces.py
@@ -70,35 +70,50 @@ def display_surfaces( grsize_scag, grsize_drfs, albedo_mcd, albedo_mod10a1, yyyy
     grsize_scag.plot( title=title, ax=axes[3] ).legend(bbox_to_anchor=(1.3,1.0))
 
 
-# In[10]:
+# In[6]:
+
+def first_and_last_column( df ):
+    return (df[[df.columns[0],df.columns[df.columns.size-1]]])
+
+
+# In[7]:
 
 (gscag_ablation_01, gdrfs_ablation_01, amcd_ablation_01, amod10_ablation_01 ) = get_surfaces( '2001', 'ablation_area' )
 (gscag_on_ice_01, gdrfs_on_ice_01, amcd_on_ice_01, amod10_on_ice_01 ) = get_surfaces( '2001', 'snow_on_ice_area' )
 (gscag_off_ice_01, gdrfs_off_ice_01, amcd_off_ice_01, amod10_off_ice_01 ) = get_surfaces( '2001', 'snow_off_ice_area' )
 
 
-# In[14]:
+# In[8]:
 
 (gscag_ablation_04, gdrfs_ablation_04, amcd_ablation_04, amod10_ablation_04 ) = get_surfaces( '2004', 'ablation_area' )
 (gscag_on_ice_04, gdrfs_on_ice_04, amcd_on_ice_04, amod10_on_ice_04 ) = get_surfaces( '2004', 'snow_on_ice_area' )
 (gscag_off_ice_04, gdrfs_off_ice_04, amcd_off_ice_04, amod10_off_ice_04 ) = get_surfaces( '2004', 'snow_off_ice_area' )
 
 
-# In[34]:
+# In[9]:
 
 #display_surfaces( gscag_ablation_01, gdrfs_ablation_01, amcd_ablation_01, amod10_ablation_01, '2001', 'Ablation Area')
 #display_surfaces( gscag_on_ice_01, gdrfs_on_ice_01, amcd_on_ice_01, amod10_on_ice_01, '2001', 'Snow on Ice Area')
 #display_surfaces( gscag_off_ice_01, gdrfs_off_ice_01, amcd_off_ice_01, amod10_ablation_01, '2001', 'Snow off Ice Area')
 
 
-# In[35]:
+# In[10]:
 
 #display_surfaces( gscag_ablation_04, gdrfs_ablation_04, amcd_ablation_04, amod10_ablation_04, '2004', 'Ablation Area')
 #display_surfaces( gscag_on_ice_04, gdrfs_on_ice_04, amcd_on_ice_04, amod10_on_ice_04, '2004', 'Snow on Ice Area')
 #display_surfaces( gscag_off_ice_04, gdrfs_off_ice_04, amcd_off_ice_04, amod10_ablation_04, '2004', 'Snow off Ice Area')
 
 
-# In[26]:
+# In[11]:
+
+#amcd_ablation_01[['ALBEDO_MCD.0030','ALBEDO_MCD.0047']]
+#amcd_ablation_01.columns.size
+#amcd_ablation_01[[amcd_ablation_01.columns[0],amcd_ablation_01.columns[amcd_ablation_01.columns.size-1]]]
+#first_and_last_column(amcd_ablation_01)
+#amcd_ablation_01[0:2]
+
+
+# In[12]:
 
 def mult_display_surfaces( d1, d2, d3, d4, d5, d6, d7, d8, yyyy1, yyyy2, surface_type):
     title1 = surface_type + ', ' + yyyy1
@@ -114,41 +129,54 @@ def mult_display_surfaces( d1, d2, d3, d4, d5, d6, d7, d8, yyyy1, yyyy2, surface
     d8.plot( title=title2, ax=axes[3,1] ).legend(bbox_to_anchor=(1.5,1.0))
 
 
-# In[30]:
+# In[13]:
 
 mult_display_surfaces( amcd_ablation_01, amod10_ablation_01, gscag_ablation_01, gdrfs_ablation_01, 
-                      amcd_ablation_04, amod10_ablation_04, gscag_ablation_04, gdrfs_ablation_04, '2001', '2004', 'Ablation Area')
+                       amcd_ablation_04, amod10_ablation_04, gscag_ablation_04, gdrfs_ablation_04, 
+                      '2001', '2004', 'Ablation Area')
 
 
-# In[32]:
+# In[14]:
 
 mult_display_surfaces( amcd_on_ice_01, amod10_on_ice_01, gscag_on_ice_01, gdrfs_on_ice_01,  
-                      amcd_on_ice_04, amod10_on_ice_04, gscag_on_ice_04, gdrfs_on_ice_04, '2001', '2004', 'Snow on Ice Area')
+                      amcd_on_ice_04, amod10_on_ice_04, gscag_on_ice_04, gdrfs_on_ice_04, 
+                      '2001', '2004', 'Snow on Ice Area')
 
 
-# In[33]:
+# In[15]:
 
 mult_display_surfaces( amcd_off_ice_01, amod10_off_ice_01, gscag_off_ice_01, gdrfs_off_ice_01,  
-                      amcd_off_ice_04, amod10_off_ice_04, gscag_off_ice_04, gdrfs_off_ice_04, '2001', '2004', 'Snow off Ice Area')
+                       amcd_off_ice_04, amod10_off_ice_04, gscag_off_ice_04, gdrfs_off_ice_04, 
+                      '2001', '2004', 'Snow off Ice Area')
 
 
-# In[36]:
+# <h3>Values when we expanded the threshold ranges:</h3>
 
-#import fnmatch
+# In[16]:
+
+mult_display_surfaces( first_and_last_column(amcd_ablation_01), first_and_last_column(amod10_ablation_01), 
+                      first_and_last_column(gscag_ablation_01), first_and_last_column(gdrfs_ablation_01), 
+                      first_and_last_column(amcd_ablation_04), first_and_last_column(amod10_ablation_04), 
+                      first_and_last_column(gscag_ablation_04), first_and_last_column(gdrfs_ablation_04),
+                      '2001', '2004', 'Ablation Area')
 
 
-# In[37]:
+# In[17]:
 
-#rootPath = "/Users/brodzik/projects/CHARIS/pdd_melt_model/2015_paper/"
-#pattern = "*" + yyyy + "*" + surface_class + "*" + surface_type + ".dat"
-#pattern
+mult_display_surfaces( first_and_last_column(amcd_on_ice_01), first_and_last_column(amod10_on_ice_01), 
+                       first_and_last_column(gscag_on_ice_01), first_and_last_column(gdrfs_on_ice_01),  
+                       first_and_last_column(amcd_on_ice_04), first_and_last_column(amod10_on_ice_04), 
+                       first_and_last_column(gscag_on_ice_04), first_and_last_column(gdrfs_on_ice_04), 
+                      '2001', '2004', 'Snow on Ice Area')
 
 
-# In[38]:
+# In[18]:
 
-#for root, dirs, files in os.walk(rootPath):
-#    for fn in fnmatch.filter(files,pattern):
-#        print(os.path.join(root,fn))
+mult_display_surfaces( first_and_last_column(amcd_off_ice_01), first_and_last_column(amod10_off_ice_01), 
+                      first_and_last_column(gscag_off_ice_01), first_and_last_column(gdrfs_off_ice_01),  
+                       first_and_last_column(amcd_off_ice_04), first_and_last_column(amod10_off_ice_04), 
+                      first_and_last_column(gscag_off_ice_04), first_and_last_column(gdrfs_off_ice_04), 
+                      '2001', '2004', 'Snow off Ice Area')
 
 
 # In[ ]:
